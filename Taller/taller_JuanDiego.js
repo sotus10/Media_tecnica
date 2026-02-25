ba550902bce5aa992bf08cbd37acfdc4411117bf
Juan Diego Soto <156706569+sotus10@users.noreply.github.com>
2026-02-25 17:50:10 -0500

    update

---
+++ Taller/taller_JuanDiego.js
@@ -0,0 +1,57 @@
+
+/*let notas = [2.5, 3.0, 4.2, 1.8, 3.7];
+let sumaMateriasPasadas = 0;
+let sumaMateriasPerdidas = 0;
+
+for(let i = 0; i < notas.length; i++){
+    if(notas[i] >= 3.0){
+        sumaMateriasPasadas += 1
+    } else {
+        sumaMateriasPerdidas += 1
+    }
+}
+
+console.log("Has pasado " + sumaMateriasPasadas + " materias, y perdiste " + sumaMateriasPerdidas);
+*/
+/*
+let usuarios = ["sofia", "mateo", "valentina"];
+// console.log(usuarios.includes("mateo"));
+let encontrado = false; 
+
+for(let i = 0; i<usuarios.length; i++){
+    if(usuarios[i] === "mateo"){
+        encontrado = true
+        console.log(encontrado)
+        break
+    }
+}
+*/
+/*
+let nombres = ["Juan", "Diego", "Soto"];
+
+function saludo(nombre){
+    return "Hola, " + nombre
+}
+
+console.log(saludo(nombres[0]));
+console.log(saludo(nombres[1]));
+console.log(saludo(nombres[2]));
+*/
+
+/*
+let preciosProductos = [1200, 3500, 800]
+
+function totalCarritodeCompras(precios) {
+    let total = 0;
+    for (let i = 0; i < precios.length; i++){
+        total += precios[i]
+    }
+    return "El precio total es " + total
+}
+
+console.log(totalCarritodeCompras(preciosProductos));
+*/
+
+let scores = [50, 120, 90, 200, 30];
+
+let maxScore = scores[0]
